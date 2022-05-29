33c06770aaeb358ced0ddb8345fe2d5c845a7e17
riteeksrivastav <riteeksrivastav@gmail.com>
2022-05-29 13:04:35 +0530

    simple producer and consumer: one producer and one consumer

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func Producer(in chan<- int, wg *sync.WaitGroup) {
+	for i := 0; i < 10; i++ {
+		in <- i
+	}
+	close(in)
+	wg.Done()
+}
+
+func Consumer(in <-chan int, wg *sync.WaitGroup) {
+	for data := range in {
+		fmt.Println(data)
+	}
+	wg.Done()
+}
+
+func main() {
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	pipe := make(chan int)
+	go Producer(pipe, wg)
+	go Consumer(pipe, wg)
+	wg.Wait()
 }
